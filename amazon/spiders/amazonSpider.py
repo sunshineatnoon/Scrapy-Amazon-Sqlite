c6a261642ed271faf3739c8f581e9d473c26ab4e
sunshineatnoon <8500575@qq.com>
2015-09-22 17:31:44 +0800

    finished for now

--- amazon/spiders/amazonSpider.py
+++ amazon/spiders/amazonSpider.py
@@ -13,16 +13,20 @@ class amazonSpider(scrapy.Spider):
                   ]
     '''
     def start_requests(self):
-        yield scrapy.Request("http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=backpack",self.parse)
+        yield scrapy.Request("http://www.amazon.com/s/ref=sr_ex_n_3?rh=n%3A7141123011%2Cn%3A10445813011%2Cn%3A9479199011%2Cn%3A360832011&bbn=10445813011&ie=UTF8&qid=1442910853&ajr=0",self.parse)
+        
         for i in range(2,20):
-            yield scrapy.Request("http://www.amazon.com/s/ref=sr_pg_2?rh=i%3Aaps%2Ck%3Abackpack&page="+str(i)+"&keywords=backpack&ie=UTF8&qid=1442907452&spIA=B00YCRMZXW,B010HWLMMA",self.parse)
-    
+            yield scrapy.Request("http://www.amazon.com/s/ref=lp_360832011_pg_2?rh=n%3A7141123011%2Cn%3A10445813011%2Cn%3A9479199011%2Cn%3A360832011&page="+str(i)+"&bbn=10445813011&ie=UTF8&qid=1442910987",self.parse)
+        
     
     
     def parse(self,response):
-        namelist = response.xpath('//a[@class="a-link-normal s-access-detail-page  a-text-normal"]/@title').extract()
-        htmllist = response.xpath('//a[@class="a-link-normal s-access-detail-page  a-text-normal"]/@href').extract()
-        imglist = response.xpath('//a[@class="a-link-normal a-text-normal"]/img/@src').extract()
+        #namelist = response.xpath('//a[@class="a-link-normal s-access-detail-page  a-text-normal"]/@title').extract()
+        #htmllist = response.xpath('//a[@class="a-link-normal s-access-detail-page  a-text-normal"]/@href').extract()
+        #imglist = response.xpath('//a[@class="a-link-normal a-text-normal"]/img/@src').extract()
+        namelist = response.xpath('//a[@class="a-link-normal s-access-detail-page s-overflow-ellipsis a-text-normal"]/@title').extract()
+        htmllist = response.xpath('//a[@class="a-link-normal s-access-detail-page s-overflow-ellipsis a-text-normal"]/@href').extract()
+        imglist = response.xpath('//img[@class="s-access-image cfMarker"]/@src').extract()
         listlength = len(namelist)
         
         for i in range(0,listlength):
