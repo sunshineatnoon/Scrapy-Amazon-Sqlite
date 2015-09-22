68bfb5ad5b0fe8ed19dd1cb2bd62a44356ef06b9
sunshineatnoon <8500575@qq.com>
2015-09-22 16:09:37 +0800

    multiple pages

--- amazon/spiders/amazonSpider.py
+++ amazon/spiders/amazonSpider.py
@@ -2,10 +2,22 @@ import scrapy
 import urllib
 from amazon.items import AmazonItem
 
+
 class amazonSpider(scrapy.Spider):
+    imgcount = 1
     name = "amazon"
     allowed_domains = ["amazon.com"]
-    start_urls = ["http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=backpack"]
+    '''
+    start_urls = ["http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=backpack",
+                  "http://www.amazon.com/s/ref=sr_pg_2?rh=i%3Aaps%2Ck%3Abackpack&page=2&keywords=backpack&ie=UTF8&qid=1442907452&spIA=B00YCRMZXW,B010HWLMMA"
+                  ]
+    '''
+    def start_requests(self):
+        yield scrapy.Request("http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=backpack",self.parse)
+        for i in range(2,20):
+            yield scrapy.Request("http://www.amazon.com/s/ref=sr_pg_2?rh=i%3Aaps%2Ck%3Abackpack&page="+str(i)+"&keywords=backpack&ie=UTF8&qid=1442907452&spIA=B00YCRMZXW,B010HWLMMA",self.parse)
+    
+    
     
     def parse(self,response):
         namelist = response.xpath('//a[@class="a-link-normal s-access-detail-page  a-text-normal"]/@title').extract()
@@ -18,6 +30,7 @@ class amazonSpider(scrapy.Spider):
             item['Name'] = namelist[i]
             item['Source'] = htmllist[i]
         
-            urllib.urlretrieve(imglist[i],"/home/xuetingli/Desktop/crawlImages/"+str(i)+".jpg")
-            item['Path'] = "/home/xuetingli/Desktop/crawlImages/"+str(i)+".jpg"
+            urllib.urlretrieve(imglist[i],"/home/xuetingli/Desktop/crawlImages/"+str(amazonSpider.imgcount)+".jpg")
+            item['Path'] = "/home/xuetingli/Desktop/crawlImages/"+str(amazonSpider.imgcount)+".jpg"
+            amazonSpider.imgcount = amazonSpider.imgcount + 1
             yield item
